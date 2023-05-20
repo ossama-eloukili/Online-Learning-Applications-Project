5ca0b6ddf93328d55d6142744a013b7d43760200
Fausto Lasca <fausto.lasca@gmail.com>
2023-05-20 15:45:08 +0200

    Created abstract UserClass

---
+++ Environment/UserClass.py
@@ -0,0 +1,12 @@
+class UserClass:
+    @abstractmethod
+    def n_clicks(self, bid):
+        pass
+    
+    @abstractmethod
+    def cum_click_cost(self, bid):
+        pass
+
+    @abstractmethod
+    def pull_user(self, price, n_users=1):
+        pass
