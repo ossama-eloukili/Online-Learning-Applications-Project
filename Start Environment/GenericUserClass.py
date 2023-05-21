4c2c1eaefb3f3ae5c965c90cbbf256d744b21de3
Fausto Lasca <fausto.lasca@gmail.com>
2023-05-21 11:30:47 +0200

    Updated GenericUserClass to accept variable deviation

--- Start Environment/GenericUserClass.py
+++ Start Environment/GenericUserClass.py
@@ -3,8 +3,12 @@ from UserClass import UserClass
 import numpy as np
 
 
+def default_dev(x):
+        return 1
+
+
 class GenericUserClass(UserClass):
-    def __init__(self, click_function, cost_function, conversion, click_dev=1, cost_dev=1):
+    def __init__(self, click_function, cost_function, conversion, click_dev=default_dev, cost_dev=default_dev):
         self.click_function = click_function
         self.click_dev = click_dev
         self.cost_function = cost_function
@@ -12,14 +16,14 @@ class GenericUserClass(UserClass):
         self.conversion = conversion
 
     def n_clicks(self, bid):
-        pull = self.click_function(bid) + np.random.normal(0, self.click_dev)
+        pull = self.click_function(bid) + np.random.normal(0, self.click_dev(bid))
         if pull > 0:
             return int(pull)
         else:
             return 0
         
     def cum_click_cost(self, bid):
-        pull = self.cost_function(bid) + np.random.normal(0, self.cost_dev)
+        pull = self.cost_function(bid) + np.random.normal(0, self.cost_dev(bid))
         if pull>0:
             return pull
         else:
@@ -37,6 +41,9 @@ class GenericUserClass(UserClass):
 if __name__ == "__main__":
     def clicks(x):
         return 10
+    
+    def dev(x):
+        return 0
 
     def cost(x):
         return x + 10
@@ -46,12 +53,12 @@ if __name__ == "__main__":
         4 : 0.5
     }
 
-    usr_class = GenericUserClass(clicks, cost, conversion, 1, 1)
+    usr_class = GenericUserClass(clicks, cost, conversion, dev, dev)
 
     for i in range(10):
         print(usr_class.n_clicks(i), usr_class.cum_click_cost(i))
 
     for i in range(10):
-        print(usr_class.pull_user(3, 10), usr_class.pull_user(4, 10))
+        print(usr_class.pull_user(3, 100), usr_class.pull_user(4, 100))
 
     print(usr_class.pull_user(5))   # This outputs an error
