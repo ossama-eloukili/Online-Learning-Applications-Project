8745d0323a53d7977f9025049e012fa3d23bfa2d
fiammetta-artioli <fiammetta1401@yahoo.it>
2023-09-19 13:22:53 +0200

    added step 6

--- new_approach/DATA_users.py
+++ new_approach/DATA_users.py
@@ -139,4 +139,55 @@ def conversion_rate_f(price):
 
 C1_NS = UserClass(F1, F2, click_f, cost_f, conversion_rate_f)
 
+#### User class C1 for step 6 ####
+F1 = 1
+F2 = 0
+
+def click_f(bid):
+    bid_0 = 2
+    bid_1 = 6
+    max_clicks = 100
+
+    # if bid < bid_0:
+    #     return 0
+    # elif bid < bid_1:
+    #     return n_max * (bid - bid_0)/(bid_1-bid_0)
+    # return n_max
+    return (bid < bid_1) * (bid > bid_0) * (bid - bid_0)/(bid_1-bid_0) * max_clicks + (bid >= bid_1) * max_clicks
+
+
+def cost_f(bid):
+    return bid * 60
+
+def conversion_rate_f(price):
+    return  np.array([(price <= 55) * 0.9 + \
+            (55 < price)*(price <= 65) * 0.6 + \
+            (65 < price)*(price <= 75) * 0.28 + \
+            (75 < price)*(price <= 85) * 0.05 + \
+            (price > 85) * 0.02, \
+            (price <= 55) * 0.6 + \
+            (55 < price)*(price <= 65) * 0.2 + \
+            (65 < price)*(price <= 75) * 0.3 + \
+            (75 < price)*(price <= 85) * 0.85 + \
+            (price > 85) * 0.25, \
+            (price <= 55) * 0.9 + \
+            (55 < price)*(price <= 65) * 0.35 + \
+            (65 < price)*(price <= 75) * 0.25 + \
+            (75 < price)*(price <= 85) * 0.1 + \
+            (price > 85) * 0.05, \
+            (price <= 55) * 0.35 + \
+            (55 < price)*(price <= 65) * 0.35 + \
+            (65 < price)*(price <= 75) * 0.15 + \
+            (75 < price)*(price <= 85) * 0.05 + \
+            (price > 85) * 0.02, \
+            (price <= 55) * 0.35 + \
+            (55 < price)*(price <= 65) * 0.8 + \
+            (65 < price)*(price <= 75) * 0.6 + \
+            (75 < price)*(price <= 85) * 0.1 + \
+            (price > 85) * 0.05])
+
+
+
+C1_NS_5_phases = UserClass(F1, F2, click_f, cost_f, conversion_rate_f)
+
 
